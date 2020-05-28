237b79d529af39570fc3f824f4cdba81a0838028
Tekraj Joshi <tekrajjoshi07@gmail.com>
2020-05-29 00:06:40 +0530

    bug in script

---
+++ Humane/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('', include('Blog.urls', namespace = 'blog')),
+    path('', include('Confession.urls', namespace = 'confession')),
+    path('accounts/', include('allauth.urls')),
+    path('chat/', include('chatapp.urls'))
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
